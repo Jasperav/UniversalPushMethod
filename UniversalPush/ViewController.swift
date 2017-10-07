05afa410adcff197c20ee7f019eb88c4d621d24b
jasperav <jasperav@hotmail.com>
2017-10-07 22:05:46 +0300

    sad

--- UniversalPush/ViewController.swift
+++ UniversalPush/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pushView: UIView!
+    var animator: UIDynamicAnimator!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.animator = UIDynamicAnimator(referenceView: self.view)
+       let dynamic = UIDynamicItemBehavior()
+        dynamic.resistance = 1
+        self.animator.addBehavior(dynamic)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+      let push = UIPushBehavior(items: [pushView], mode: .instantaneous)
+        push.magnitude = 0.15
+        
+        animator.addBehavior(push)
     }
-
-
 }
 
